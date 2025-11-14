217049a1f73ac94c2f003a398b59d077f65307ae
marc55s <marc.schoenig@gmail.com>
2025-11-14 22:28:44 +0100

    refactor: main

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,7 @@
-#![cfg(feature = "smart-leds-trait")]
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_sys::*;
 use smart_leds::hsv::{hsv2rgb, Hsv};
-use smart_leds_trait::SmartLedsWrite;
+use smart_leds::SmartLedsWrite;
 use std::thread::sleep;
 use std::time::Duration;
 use ws2812_esp32_rmt_driver::Ws2812Esp32Rmt;
@@ -13,24 +12,24 @@ fn main() -> ! {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
-    let led_pin = peripherals.pins.gpio33;
+    let led_pin = peripherals.pins.gpio33; // GPIO 33
     let channel = peripherals.rmt.channel0;
     let mut ws2812 = Ws2812Esp32Rmt::new(channel, led_pin).unwrap();
 
     println!("Start NeoPixel rainbow!");
 
-    let mut hue = unsafe { esp_random() } as u8;
+    let hue = unsafe { esp_random() } as u8;
     loop {
         let pixels = std::iter::repeat(hsv2rgb(Hsv {
             hue,
             sat: 255,
             val: 8,
         }))
-        .take(300);
+        .take(25);
         ws2812.write(pixels).unwrap();
 
         sleep(Duration::from_millis(500));
 
-            hue = hue.wrapping_add(5);
+        // hue = hue.wrapping_add(5);
     }
 }
