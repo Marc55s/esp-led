78db3b6d878bca8fc72b29b1c803a2dfb206a84f
marc55s <marc.schoenig@gmail.com>
2025-11-15 02:29:32 +0100

    feat: wifi, led

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod wifi;
+
+use crate::wifi::wifi_setup;
+
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_sys::*;
 use smart_leds::hsv::{hsv2rgb, Hsv};
@@ -6,30 +10,35 @@ use std::thread::sleep;
 use std::time::Duration;
 use ws2812_esp32_rmt_driver::Ws2812Esp32Rmt;
 
-fn main() -> ! {
+fn main() -> anyhow::Result<()> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
 
-    let peripherals = Peripherals::take().unwrap();
-    let led_pin = peripherals.pins.gpio33; // GPIO 33
-    let channel = peripherals.rmt.channel0;
-    let mut ws2812 = Ws2812Esp32Rmt::new(channel, led_pin).unwrap();
+    wifi_setup().expect("Wifi setup failed");
 
-    println!("Start NeoPixel rainbow!");
-
-    let hue = unsafe { esp_random() } as u8;
-    loop {
-        let pixels = std::iter::repeat(hsv2rgb(Hsv {
-            hue,
-            sat: 255,
-            val: 8,
-        }))
-        .take(25);
-        ws2812.write(pixels).unwrap();
-
-        sleep(Duration::from_millis(500));
-
-        // hue = hue.wrapping_add(5);
-    }
+    // let peripherals = Peripherals::take().unwrap();
+    // let led_pin = peripherals.pins.gpio33; // GPIO 33
+    // let channel = peripherals.rmt.channel0;
+    // let mut ws2812 = Ws2812Esp32Rmt::new(channel, led_pin).unwrap();
+    //
+    // println!("Start NeoPixel rainbow!");
+    //
+    // let hue = unsafe { esp_random() } as u8;
+    //
+    //
+    // loop {
+    //     let pixels = std::iter::repeat(hsv2rgb(Hsv {
+    //         hue,
+    //         sat: 255,
+    //         val: 8,
+    //     }))
+    //     .take(25);
+    //     ws2812.write(pixels).unwrap();
+    //
+    //     sleep(Duration::from_millis(500));
+    //
+    //     // hue = hue.wrapping_add(5);
+    // }
+    Ok(())
 }
